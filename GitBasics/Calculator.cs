3ce8c6d6fe23b2fbf9acd936de29747dd50cb10e
Giame Fajardo <giame_fajardo@cecomsa.com>
2022-05-14 10:06:58 -0400

    Add Function added

--- GitBasics/Calculator.cs
+++ GitBasics/Calculator.cs
@@ -6,5 +6,9 @@ namespace GitBasics
 {
     internal class Calculator
     {
+        public static float Add(float numberOne, float NumberTwo)
+        {
+            return numberOne + NumberTwo;
+        }
     }
 }
